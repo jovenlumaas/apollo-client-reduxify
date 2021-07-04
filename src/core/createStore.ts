38567660ac7d3b804ad4f7770e86794b7417508c
jovenlumaas <jovenlumaas@gmail.com>
2021-07-05 00:31:14 +0800

    update 'createReactiveStore'

--- src/core/createStore.ts
+++ src/core/createStore.ts
@@ -1,9 +1,6 @@
-import type { TMergedReactiveVars } from "../types";
+import type { TMergedReactiveVars } from '../types';
 
-type TDispatchFn<T extends Record<string, any>> = <A extends keyof T>(
-  type: A,
-  ...args: Parameters<T[A]>
-) => void;
+type TDispatchFn<T extends Record<string, any>> = <A extends keyof T>(type: A, ...args: Parameters<T[A]>) => void;
 
 type TGetStateFn<T extends Record<string, any>> = <K extends keyof T>(
   selector: K,
@@ -12,16 +9,16 @@ type TGetStateFn<T extends Record<string, any>> = <K extends keyof T>(
 
 type TCreateStoreFn = <
   M extends TMergedReactiveVars<any, any>,
-  RV = M["reactiveVars"],
-  FT = M["fieldTypes"],
-  R = M["reducers"],
-  S = M["selectors"]
+  RV = M['reactiveVars'],
+  FT = M['fieldTypes'],
+  R = M['reducers'],
+  S = M['selectors'],
 >(
   options: M,
   developerOptions?: {
-    enableLog?: boolean | (keyof M["reactiveVars"])[];
+    enableLog?: boolean | (keyof M['reactiveVars'])[];
     context?: Record<string, any>;
-  }
+  },
 ) => {
   reactiveVars: RV;
   fieldTypes: FT;
@@ -34,15 +31,15 @@ type TCreateStoreFn = <
 
 export const createReactiveVarStore: TCreateStoreFn = (
   { reactiveVars, fieldTypes, reducers, selectors, nonHookSelectors },
-  developerOptions
+  developerOptions,
 ) => {
   const { enableLog, context } = developerOptions ?? {};
   const allReducers = Object.keys(reducers).reduce(
     (acc, key) => ({
       ...acc,
-      [key]: reducers[key]({ reactiveVars, enableLog, context }),
+      [key]: reducers[key]({ reactiveVars: { ...reactiveVars }, enableLog, context }),
     }),
-    {} as any
+    {} as any,
   );
 
   return {
@@ -57,9 +54,7 @@ export const createReactiveVarStore: TCreateStoreFn = (
       return selectors[selector as keyof typeof selectors](args[0]);
     },
     readReactiveVar: (selector, ...args) => {
-      return nonHookSelectors[selector as keyof typeof nonHookSelectors](
-        args[0]
-      );
+      return nonHookSelectors[selector as keyof typeof nonHookSelectors](args[0]);
     },
   };
 };
